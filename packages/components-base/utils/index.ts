d75e382138168780ae3495eea4833393492a4b4d
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2020-10-08 21:28:37 +0700

    feat(components-base): add style utils var css

--- packages/components-base/utils/index.ts
+++ packages/components-base/utils/index.ts
@@ -1,1 +1,6 @@
+export { default as Collapse } from "./Collapse.svelte";
+export { default as Scrollbar } from "./Scrollbar.svelte";
+export * as actions from "./actions";
 export * from "./generate";
+export * from "./style";
+export * from "./popper";
