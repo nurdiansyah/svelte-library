722e4b909c833bcc9e5fbc0ee95d8d37b76e47fb
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2020-10-11 06:57:49 +0700

    save

---
+++ packages/components-base/data-display/DeboxIcon.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import clsx from "clsx";
+
+  let className: string | null = null,
+    classes: string,
+    fontSize: string | null;
+  export { className as class };
+  export let name: string | null = null;
+  export let size = null;
+  export let color = null;
+  $: classes = clsx("dbx-icon", className, `-${name}`);
+  $: fontSize = typeof size === "number" ? `${size}rem` : size;
+</script>
+
+<i class="{classes}" style="--icon-size: {fontSize};--icon-color: {color};"></i>
+
+<style src="./styles/deboxicon.scss" global>
+</style>
--- packages/components-base/data-display/IconBurger.svelte
+++ packages/components-base/data-display/IconBurger.svelte
@@ -3,13 +3,11 @@
   export let active: boolean = false;
 </script>
 
+<div class="burger-icon {type !== 'default' && `-${type}`}" class:-active="{active}"><span></span></div>
+
 <style lang="scss" global>
   @use "../scss/mixins/burger-icon" as *;
   :local(.burger-icon) {
     @include create-burger-icon();
   }
 </style>
-
-<div class="burger-icon {type !== 'default' && `-${type}`}" class:-active="{active}">
-  <span></span>
-</div>
