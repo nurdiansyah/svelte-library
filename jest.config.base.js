549a3cec7d0ca12db1d8175626673fef00d9264a
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2020-10-15 19:16:29 +0700

    chore: fix setting

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,10 +1,18 @@
 module.exports = {
+  globals: {
+    "svelte-jester": {
+      // Let's avoid type checking during tests (performance boost).
+      diagnostics: false
+    }
+  },
   transform: {
     "^.+\\.[jt]sx?$": "@sucrase/jest-plugin",
-    "^.+\\.svelte$": ["svelte-jester", { preprocess: true }]
+    "^.+\\.svelte$": ["svelte-jester", { preprocess: true, rootMode: "upward" }]
   },
   testRegex: "(/(tests|__tests__)/.*.(test|spec)).(jsx?|tsx?)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node", "svelte"],
   coveragePathIgnorePatterns: ["((tests|__tests__)/.*.mock).(jsx?|tsx?)$"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom", "@testing-library/jest-dom/extend-expect"],
+  transformIgnorePatterns: ["node_modules/(?!(@popperjs)/)"],
   bail: false
 };
