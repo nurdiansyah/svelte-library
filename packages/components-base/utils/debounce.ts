5016740da33be842f7714e31d781ba7e535b5c3a
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2020-10-27 05:30:18 +0700

    change default export debounce

--- packages/components-base/utils/debounce.ts
+++ packages/components-base/utils/debounce.ts
@@ -1,6 +1,6 @@
 // Corresponds to 10 frames at 60 Hz.
 // A few bytes payload overhead when lodash/debounce is ~3 kB and debounce ~300 B.
-export default function debounce(func: (arg0: any, arg1: any[]) => void, wait = 166) {
+export function debounce(func: (arg0: any, arg1: any[]) => void, wait = 166) {
   let timeout: NodeJS.Timeout;
   function debounced(...args: any[]) {
     // eslint-disable-next-line consistent-this
