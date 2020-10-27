bc3ce241740696815803de7546e2e5cf6f8cd43c
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2020-10-27 13:58:31 +0700

    fix clean

--- packages/components-base/utils/generate.d.ts
+++
@@ -1,6 +0,0 @@
-interface Options {
-    size?: number;
-    prefix?: string;
-}
-export declare const getId: ({ prefix, size }: Options) => string;
-export {};
--- packages/components-base/utils/generate.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getId = void 0;
-const core_web_1 = require("@deboxsoft/core-web");
-exports.getId = ({ prefix = "dbx", size = 5 }) => {
-    return `${prefix}-${core_web_1.utils.generateId(size)}`;
-};
